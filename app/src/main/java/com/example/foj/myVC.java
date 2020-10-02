5589957074821c90af23c4cef6e20fc9a446f372
nykrlab <70588800+nykrlab@users.noreply.github.com>
2020-10-02 23:33:29 +0900

    Create myVC.java

---
+++ app/src/main/java/com/example/foj/myVC.java
@@ -0,0 +1,18 @@
+package com.example.foj;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
+
+public class myVC extends Activity {
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.myvc);
+
+    }
+}
