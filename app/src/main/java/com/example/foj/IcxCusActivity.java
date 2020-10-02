e2b1a0f6fc550170fafe3f15304561438a52244f
JoonPark0221 <tmdgh0221@gmail.com>
2020-10-02 16:57:49 +0900

    Add icx address register

--- app/src/main/java/com/example/foj/IcxCusActivity.java
+++ app/src/main/java/com/example/foj/IcxCusActivity.java
@@ -4,10 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import foundation.icon.icx.KeyWallet;
+import foundation.icon.icx.data.Bytes;
 
 public class IcxCusActivity extends AppCompatActivity {
+    /*seungho */
+    EditText icxAddress;
+    /* seungho */
     Button done;
 
     @Override
@@ -15,7 +23,16 @@ public class IcxCusActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.four_icx_cus);
 
+        /* seungho */
+        icxAddress = findViewById(R.id.username);
+        /* seungho */
         done = findViewById(R.id.done);
+
+        /* seungho */
+        KeyWallet cusWallet = KeyWallet.load(new Bytes("a03694b07ab48f01e8f617e4c2d3953fcae8a0347d17a14a426030ff0de05e41"));
+        icxAddress.setText(cusWallet.getAddress().toString());
+        Log.i("### cusWallet Address", icxAddress.getText().toString());
+        /* seungho */
 
         done.setOnClickListener(new View.OnClickListener() {
             @Override
