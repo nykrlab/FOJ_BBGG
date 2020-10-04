4eec8d14870fa5224cf81bb28937d6afd83f5074
JoonPark0221 <tmdgh0221@gmail.com>
2020-10-04 23:04:41 +0900

    Modify account page to activate

--- app/src/main/java/com/example/foj/LoginActivity.java
+++ app/src/main/java/com/example/foj/LoginActivity.java
@@ -24,7 +24,7 @@ public class LoginActivity extends AppCompatActivity {
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent (LoginActivity.this,LoginActivity.class);
+                Intent intent = new Intent (LoginActivity.this,MyaccountActivity.class);
                 startActivity(intent);
             }
         });
