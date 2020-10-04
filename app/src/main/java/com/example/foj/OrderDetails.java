453e384a5df739ed240c1b6b588049f35d353d3f
nykrlab <70588800+nykrlab@users.noreply.github.com>
2020-10-04 17:46:24 +0900

    Create OrderDetails.java

---
+++ app/src/main/java/com/example/foj/OrderDetails.java
@@ -0,0 +1,21 @@
+package com.example.foj;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+
+public class OrderDetails extends Activity {
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.orderdetails);
+
+
+    }
+}
