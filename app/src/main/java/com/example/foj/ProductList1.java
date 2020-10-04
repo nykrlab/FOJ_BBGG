76cb97ca6fdbf21e622f8c2ab50e5f42cda453b0
nykrlab <70588800+nykrlab@users.noreply.github.com>
2020-10-04 17:47:56 +0900

    Create ProductList1.java

---
+++ app/src/main/java/com/example/foj/ProductList1.java
@@ -0,0 +1,29 @@
+package com.example.foj;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+public class ProductList1 extends Activity {
+    Button btnplus;
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.productlist1);
+
+        btnplus = findViewById(R.id.btnplus);
+
+        btnplus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (ProductList1.this,AddProduct.class);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+}
