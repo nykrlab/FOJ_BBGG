34ebc8b687662db51319ee7b6c60d0dccb38745e
nykrlab <70588800+nykrlab@users.noreply.github.com>
2020-10-04 17:45:23 +0900

    Create myVP.java

---
+++ app/src/main/java/com/example/foj/myVP.java
@@ -0,0 +1,18 @@
+package com.example.foj;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
+
+public class myVP extends Activity {
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.myvp);
+
+    }
+}
