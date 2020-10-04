1374b8a30348ccbbfeaffee58e8fdfb3376eb6c2
JoonPark0221 <tmdgh0221@gmail.com>
2020-10-04 16:19:49 +0900

    Add issue VC implementation

--- app/src/main/java/com/example/foj/MyaccountActivity.java
+++ app/src/main/java/com/example/foj/MyaccountActivity.java
@@ -2,23 +2,55 @@ package com.example.foj;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.concurrent.ExecutionException;
+
+import foundation.icon.icx.IconService;
+import foundation.icon.icx.KeyWallet;
+import foundation.icon.icx.data.Bytes;
+import foundation.icon.icx.transport.http.HttpProvider;
+
+import static java.lang.Math.pow;
+
 public class MyaccountActivity extends Activity {
+    /* seungho */
+    TextView tvbalance;
+    /* seungho */
     Button btnprfl, btnprdct, btnordr, btnpymnt;
 
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.myaccount);
 
+        /* seungho */
+        tvbalance = findViewById(R.id.balance);
+        /* seungho */
         btnprfl = findViewById(R.id.btnprfl);
         btnprdct = findViewById(R.id.btnprdct);
         btnordr = findViewById(R.id.btnordr);
         btnpymnt = findViewById(R.id.btnpymnt);
+
+        /* seungho */
+        try {
+            String balance = new getBalance().execute().get();
+            tvbalance.setText(balance);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+        /* seungho */
 
 
         btnprfl.setOnClickListener(new View.OnClickListener() {
@@ -51,4 +83,30 @@ public class MyaccountActivity extends Activity {
         });
 
     }
+
+    /* seungho */
+    class getBalance extends AsyncTask<Void, Void, String> {
+
+        HttpProvider httpProvider = new HttpProvider("https://bicon.net.solidwallet.io/api/v3");
+        IconService iconService = new IconService(httpProvider);
+        KeyWallet cusWallet = KeyWallet.load(new Bytes("a03694b07ab48f01e8f617e4c2d3953fcae8a0347d17a14a426030ff0de05e41"));
+
+        @Override
+        protected String doInBackground(Void... voids) {
+
+            try {
+
+                BigDecimal balanceLoop = BigDecimal.valueOf(iconService.getBalance(cusWallet.getAddress()).execute().doubleValue());
+                BigDecimal balanceIcx = balanceLoop.divide(BigDecimal.valueOf(pow(10, 18)));
+                Log.i("### balanceIcx", balanceIcx.toString());
+                return balanceIcx.toString();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+    }
+    /* seungho */
 }
--- app/src/main/java/com/example/foj/myVC.java
+++ app/src/main/java/com/example/foj/myVC.java
@@ -3,16 +3,31 @@ package com.example.foj;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.annotation.Nullable;
 
 public class myVC extends Activity {
 
+    /* seungho */
+    EditText etVC;
+    /* seungho */
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.myvc);
+
+        /* seungho */
+        Intent intent = getIntent();
+        String VC = intent.getExtras().getString("VC");
+        Log.i("### VC", VC);
+
+        etVC = findViewById(R.id.username);
+        etVC.setText(VC);
+        /* seungho */
 
     }
 }
