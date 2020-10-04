5ffecc7d8a5c06e7964e47901f372a59a14a4cfc
JoonPark0221 <tmdgh0221@gmail.com>
2020-10-04 22:52:06 +0900

    Add issue vp function

--- app/src/main/java/com/example/foj/vctoVP.java
+++ app/src/main/java/com/example/foj/vctoVP.java
@@ -2,30 +2,97 @@ package com.example.foj;
 
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.bouncycastle.util.encoders.Hex;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+import foundation.icon.did.Presentation;
+import foundation.icon.did.core.Algorithm;
+import foundation.icon.did.core.AlgorithmProvider;
+import foundation.icon.did.core.DidKeyHolder;
+import foundation.icon.did.core.KeyProvider;
+import foundation.icon.did.exceptions.AlgorithmException;
+import foundation.icon.did.protocol.ClaimRequest;
 
 public class vctoVP extends AppCompatActivity {
     Button btnVP;
+    /* seungho */
+    String VP;
+    ArrayList<String> items;
+    DatabaseReference mPostReference;
+    /* seungho */
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.vctovp);
 
-        btnVP = (Button) findViewById(R.id.btnVP);
+        /* seungho */
+        items = new ArrayList<>();
+        mPostReference = FirebaseDatabase.getInstance().getReference().child("did:icon:03:0x987654321");
+        mPostReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
 
+                items.clear();
+                for(DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
+                    String item = (String) postSnapshot.getValue();
+                    items.add(item);
+                }
+
+                try {
+                    VP = new issueVP(items).execute().get();
+                } catch (ExecutionException e) {
+                    e.printStackTrace();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {}
+        });
+        /* seungho */
+
+        btnVP = (Button) findViewById(R.id.btnVP);
         btnVP.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(vctoVP.this,myVP.class);
-                startActivity(intent);
+
+                /* seungho */
+                if (VP == null) {
+                    Toast.makeText(getApplicationContext(), "Just wait a moment please", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Intent intent = new Intent(vctoVP.this,myVP.class);
+                    intent.putExtra("VP", VP);
+                    startActivity(intent);
+                }
+                /* seungho */
+
             }
         });
 
@@ -42,6 +109,62 @@ public class vctoVP extends AppCompatActivity {
     // context - 어플리케이션 전역 환경 정보에 대한 Interface // 여기서는 this
     // resource - view로 매핑될 리소스 id / textview 위젯으로 구성된 ListView 아이템 리소스 id
     // objects - 배열로 선언된 사용자 데이터
-    final String[] subject_select = {"회사명","회사 주소","사업자번호","생산보고서","회사 ICX 계좌","책임 매니저 이름","매니저 메일","상세 설명"};
+    final String[] subject_select = {"농가명","농가 주소","사업자번호","농가 이메일","생산보고서","농가 ICX 계좌","상세 설명"};
 
+    /* seungho */
+    class issueVP extends AsyncTask<Void, Void, String> {
+
+        Algorithm algorithm = AlgorithmProvider.create(AlgorithmProvider.Type.ES256K);
+        ArrayList<String> VCs;
+
+        issueVP(ArrayList<String> VCs) {
+            this.VCs = VCs;
+        }
+
+        @Override
+        protected String doInBackground(Void... voids) {
+
+            try {
+
+                String holderDid = "did:icon:03:0x987654321";
+                Date requestDate = new Date();
+                List<String> claimTypes = Arrays.asList("email");
+                String nonce = Hex.toHexString(AlgorithmProvider.secureRandom().generateSeed(4));
+
+                ClaimRequest request = new ClaimRequest.Builder(ClaimRequest.Type.PRESENTATION)
+                        .algorithm(AlgorithmProvider.Type.NONE)
+                        .responseId(holderDid)
+                        .requestDate(requestDate)
+                        .requestClaimTypes(claimTypes)
+                        .nonce(nonce)
+                        .build();
+                String unsignedJwt = request.compact();
+
+                request = ClaimRequest.valueOf(unsignedJwt);
+
+                holderDid = "did:icon:03:0x987654321";
+                String holderKeyId = "Holder-key";
+                KeyProvider holderKeyProvider = algorithm.generateKeyProvider(holderKeyId);
+                DidKeyHolder holderKeyHolder = new DidKeyHolder.Builder(holderKeyProvider)
+                        .did(holderDid)
+                        .build();
+                Presentation presentation = new Presentation.Builder()
+                        .didKeyHolder(holderKeyHolder)
+                        .nonce(request.getNonce())
+                        .build();
+                for (int i = 0; i < VCs.size(); i++) {
+                    presentation.addCredential(VCs.get(i));
+                }
+
+                String signedPresentation = holderKeyHolder.sign(presentation.buildJwt());
+                return signedPresentation;
+
+            } catch (AlgorithmException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+    }
+    /* seungho */
 }
