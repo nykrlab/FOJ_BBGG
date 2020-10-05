91538b39fbd9159c98b14e41e8e68e30ca790fdc
nykrlab <70588800+nykrlab@users.noreply.github.com>
2020-10-06 07:18:48 +0900

    Update OrderList.java

--- app/src/main/java/com/example/foj/OrderList.java
+++ app/src/main/java/com/example/foj/OrderList.java
@@ -2,22 +2,26 @@ package com.example.foj;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 
 public class OrderList extends Activity {
-    LinearLayout btnhlb;
+    LinearLayout btnhlb_c;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.orderlist);
 
-        btnhlb = findViewById(R.id.btnhlb);
+        btnhlb_c = findViewById(R.id.btnhlb_c);
 
-        btnhlb.setOnClickListener(new View.OnClickListener() {
+
+        btnhlb_c.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent (OrderList.this,OrderDetails.class);
