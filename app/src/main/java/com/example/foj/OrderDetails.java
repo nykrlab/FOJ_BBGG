aed29eb3c755c405ef1b339aeab30e8fc2c861cc
nykrlab <70588800+nykrlab@users.noreply.github.com>
2020-10-06 07:18:26 +0900

    Update OrderDetails.java

--- app/src/main/java/com/example/foj/OrderDetails.java
+++ app/src/main/java/com/example/foj/OrderDetails.java
@@ -12,9 +12,20 @@ import android.widget.LinearLayout;
 
 public class OrderDetails extends Activity {
 
+    ImageView btnbuy2;
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.orderdetails);
+
+        btnbuy2=findViewById(R.id.btnbuy2);
+        btnbuy2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(OrderDetails.this,Confirm.class);
+                startActivity(intent);
+            }
+        });
 
 
     }
